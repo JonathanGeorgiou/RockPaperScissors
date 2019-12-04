b98df3522635072d23c732089e4bae0af4da3aa2
JonathanGeorgiou <j.georgiou94@gmail.com>
2019-12-04 10:33:07 +0000

    fourth commit - logic checks out

--- src/main/java/com/qa/rockpaperscissors/RockPaperScissors.java
+++ src/main/java/com/qa/rockpaperscissors/RockPaperScissors.java
@@ -3,7 +3,20 @@ package com.qa.rockpaperscissors;
 public class RockPaperScissors {
 
 	public String play(State computerChoice, State userChoice) {
-		return null;
+		if (computerChoice.equals(State.SCISSORS) && userChoice.equals(State.ROCK)) {
+			return "User Wins";
+		}
+		if (computerChoice.equals(State.ROCK) && userChoice.equals(State.PAPER)) {
+			return "User Wins";
+		}
+		if (computerChoice.equals(State.PAPER) && userChoice.equals(State.SCISSORS)) {
+			return "User Wins";
+		}
+		if (computerChoice.equals(userChoice)) {
+			return "Draw";
+		} else {
+			return "Computer Wins";
+		}
 
 	}
 
